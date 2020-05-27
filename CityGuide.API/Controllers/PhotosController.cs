f63a070ef72d03c57020cf33efdfb2b7531d52a9
Mehmet Özgür Aslan <mehmetozguraslan09@gmail.com>
2020-05-27 19:00:06 +0300

    PhotosController gelistirmeleri

---
+++ CityGuide.API/Controllers/PhotosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using CityGuide.API.Data;
+using CityGuide.API.Dtos;
+using CityGuide.API.Helpers;
+using CityGuide.API.Models;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CityGuide.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]    
+    [Route("api/cities/{cityId}/photos")]
+    public class PhotosController : ControllerBase
+    {
+        private IAppRepository _appRepository;
+        private IMapper _mapper;
+        IOptions<CloudinarySettings> _cloudinaryConfig;
+
+        private Cloudinary _cloudinary;
+
+        public PhotosController(IAppRepository appRepository, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            this._appRepository = appRepository;
+            this._mapper = mapper;
+            this._cloudinaryConfig = cloudinaryConfig;
+
+            Account account = new Account(this._cloudinaryConfig.Value.CloudName, this._cloudinaryConfig.Value.ApiKey, this._cloudinaryConfig.Value.ApiSecret);
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        [HttpPost]
+        public ActionResult AddPhotoForCity(int cityId, [FromForm]PhotoForCreationDto photoForCreationDto)
+        {
+            var city = _appRepository.GetCityById(cityId);
+
+            if (city == null)
+            {
+                return BadRequest("Could not find the city.");
+            }
+
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            if (currentUserId != city.UserId)
+            {
+                return Unauthorized();
+            }
+
+            var file = photoForCreationDto.File;
+
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    var uploadParams = new ImageUploadParams
+                    {
+                        File = new FileDescription(file.Name, stream)
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photoForCreationDto.Url = uploadResult.Uri.ToString();
+            photoForCreationDto.PublicId = uploadResult.PublicId;
+
+            var photo = _mapper.Map<Photo>(photoForCreationDto);
+            photo.City = city;
+
+            if (!city.Photos.Any(p => p.IsMain))
+            {
+                photo.IsMain = true;
+            }
+
+            city.Photos.Add(photo);
+
+            if (_appRepository.SaveAll())
+            {
+                var photoToReturn = _mapper.Map<PhotoForReturnDto>(photo);
+
+                return CreatedAtRoute("GetPhoto", new { id = photo.Id }, photoToReturn);
+            }
+
+            return BadRequest("Could not add the photo");
+        }
+
+        [HttpGet("{id}", Name = "GetPhoto")]
+        public ActionResult GetPhoto(int id)
+        {
+            var photoFromDb = _appRepository.GetPhoto(id);
+
+            var photo = _mapper.Map<PhotoForReturnDto>(photoFromDb);
+
+            return Ok(photo);
+        }
+    }
+}
